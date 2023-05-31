f22541c4adf29c09ff8940bbfb25194048eefe85
MOFIJUL HAQUE <mofijulhoque995@gmail.com>
2023-05-31 21:07:30 +0530

    31 May - 2023

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDM-cIHy265GEOX_dmDDB7P3DuUq4O9CCI",
+  authDomain: "clone-82baa.firebaseapp.com",
+  projectId: "clone-82baa",
+  storageBucket: "clone-82baa.appspot.com",
+  messagingSenderId: "341575116077",
+  appId: "1:341575116077:web:b59d7dd0c4a47586131fa4",
+  measurementId: "G-XSNNHKC7QW"
+};
